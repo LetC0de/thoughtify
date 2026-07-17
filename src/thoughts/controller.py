b2e6761c6e5a27caf96e7a706eb21fedcd749dfa
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-17 19:10:46 +0530

    feat: enhance thought creation with database integration

--- src/thoughts/controller.py
+++ src/thoughts/controller.py
@@ -1,5 +1,17 @@
 from src.thoughts.schema import thought_schema
+from sqlalchemy.orm import Session
+from src.thoughts.model import thought_model
 
-def create_thought(body: thought_schema):
-    print(body.model_dump())
-    return {"message": "Thought created Successfully"}
+def create_thought(body: thought_schema,db:Session):
+
+    data = body.model_dump()
+
+    new_thought = thought_model(title=data["title"],
+                                content=data["content"])
+
+    db.add(new_thought)
+    db.commit()
+    db.refresh(new_thought)
+
+
+    return {"message": "Thought created Successfully","data":new_thought}
--- src/thoughts/router.py
+++ src/thoughts/router.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from src.thoughts import controller
 from src.thoughts.schema import thought_schema
+from src.utils.db import get_db
 
 thought_router = APIRouter(prefix="/thought")
 
 
 @thought_router.post("/create")
-def create_thought(body:thought_schema):
-    return controller.create_thought(body)
+def create_thought(body:thought_schema,db=Depends(get_db)):
+    return controller.create_thought(body,db)
