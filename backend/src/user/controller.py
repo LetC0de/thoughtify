08025c0cba88df66674dd1199eb739d3c376fd17
LetC0de <abhishekjiiofficial86@gmail.com>
2026-07-20 13:53:03 +0530

    feat: add user login functionality with JWT token generation and update user schema

--- backend/src/user/controller.py
+++ backend/src/user/controller.py
@@ -1,8 +1,10 @@
+from src.user.schema import UserSchema, UserLoginSchema
+from fastapi import HTTPException, status
+from datetime import datetime, timedelta
+from src.utils.settings import settings
 from sqlalchemy.orm import Session
-from src.user.schema import UserSchema
-from fastapi import HTTPException
 from pwdlib import PasswordHash
-
+import jwt
 
 password_hash = PasswordHash.recommended()
 
@@ -39,3 +41,24 @@ def register_user(body: UserSchema, db: Session):
     db.refresh(new_user)
 
     return new_user
+
+
+
+def login_user(body:UserLoginSchema, db:Session):
+
+    user = db.query(UserSchema).filter(UserSchema.username == body.username).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+
+    if not password_hash.verify(body.password, user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
+    
+    exp_time = datetime.now() + timedelta(minutes=settings.EXP_TIME)
+
+    token = jwt.encode({
+        "_id": user.id,
+        "exp": exp_time.timestamp()
+    }, settings.SECRET_KEY,settings.ALGORITHM)
+
+    return {"token": token, "user": user}
--- backend/src/user/router.py
+++ backend/src/user/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends,status
 from sqlalchemy.orm import Session
 from src.user import controller
-from src.user.schema import UserSchema, UserResponseSchema
+from src.user.schema import UserSchema, UserResponseSchema, UserLoginSchema
 from src.utils.db import get_db
 
 
@@ -9,6 +9,11 @@ user_router = APIRouter(prefix="/user")
 
 
 
-user_router.post("/register", response_model=UserResponseSchema, status_code=status.HTTP_201_CREATED)
+@user_router.post("/register", response_model=UserResponseSchema, status_code=status.HTTP_201_CREATED)
 def register_user(body: UserSchema, db: Session = Depends(get_db)):
     return controller.register_user(body, db)
+
+
+@user_router.post("/login", response_model=UserResponseSchema, status_code=status.HTTP_200_OK)
+def login_user(body:UserLoginSchema, db:Session = Depends(get_db)):
+    return controller.login_user(body, db)
